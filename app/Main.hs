2536d0b8a9b766515dadf27029d2c27a2923a3d4
fneco <izumi.f.tech@gmail.com>
2023-12-25 23:05:13 +0900

    Main.hs のみで完結させる

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main (main) where
 
-import Lib (compareNumbers)
 import System.Random
+import Text.Read (readMaybe)
 
 main :: IO ()
 main = do
@@ -12,3 +12,30 @@ main = do
   putStrLn ("The secret number is: " ++ show secretNumber)
 
   compareNumbers secretNumber
+
+compareNumbers :: Int -> IO ()
+compareNumbers secretNumber = do
+  putStrLn "Please input your guess."
+
+  guess <- getLine
+
+  let maybeGuessedNumber = readMaybe guess :: Maybe Int
+  case maybeGuessedNumber of
+    Nothing -> compareNumbers secretNumber
+    Just guessedNumber -> do
+      putStrLn ("You guessed: " ++ guess)
+
+      let ordering = compare guessedNumber secretNumber
+
+      isWin <- case ordering of
+        LT -> do
+          putStrLn "Too small!"
+          return False
+        GT -> do
+          putStrLn "Too big!"
+          return False
+        EQ -> return True
+
+      if isWin
+        then putStrLn "You win!"
+        else compareNumbers secretNumber
